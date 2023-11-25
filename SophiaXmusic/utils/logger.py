523ba219ed0043866a58bcb4ef118d1aac592a27
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-25 20:55:46 +0530

    Update logger.py

--- SophiaXmusic/utils/logger.py
+++ SophiaXmusic/utils/logger.py
@@ -8,18 +8,17 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>`{message.chat.id}`</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> `{message.chat.title}`
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
-
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>`{message.from_user.id}`</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
-
-<b>ǫᴜᴇʀʏ :</b> `{message.text.split(None, 1)[1]}`
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> `{streamtype}`"""
+╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
+◈ <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+◈ 𝐂𝐡𝐚𝐭 ➪ **{message.chat.title}**
+◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ `{message.chat.id}`
+◈ 𝐔𝐬𝐞𝐫 ➪ **{message.from_user.mention}**
+◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ **@{message.from_user.username}**
+◈ 𝐈𝐝 ➪ `{message.from_user.id}`
+◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ **{chatusername}**
+◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ `{message.text.split(None, 1)[1]}`
+◈ 𝐁𝐲 ➪ **{streamtype} ▄ █ ▄ █ ▄**
+╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
