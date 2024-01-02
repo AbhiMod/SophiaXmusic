fcab590ae94bfe51783f1ac9d20cbfad95510fce
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-02 19:39:44 +0530

    Create support.py

---
+++ SophiaXmusic/plugins/tools/support.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+import config import 
+from SophiaXmusic import app
+
+OP = "-1001841879487"
+
+async def new_message(chat_id: int, message: str, reply_markup=None):
+    await app.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
+
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(client: Client, message: Message):
+    if (await client.get_me()).id == message.left_chat_member.id:
+        remove_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        title = message.chat.title
+        username = f"@{message.chat.username}"
+        chat_id = message.chat.id
+        rirurubye = f"✫ <b><u>ʟᴇғᴛ ɢʀᴏᴜᴘ</u></b> :\n\nᴄʜᴀᴛ ɪᴅ : {chat_id}\nᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ : {username}\nᴄʜᴀᴛ ᴛɪᴛʟᴇ : {title}\n\nʀᴇᴍᴏᴠᴇᴅ ʙʏ : {remove_by}"
+        reply_markup = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            message.from_user.first_name,
+            user_id=message.from_user.id
+        )
+    ]
+])
+
+        
+        await new_message(OP, rirurubye, reply_markup)
